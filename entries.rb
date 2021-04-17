fa68cdc01a5714a7d43639e1798cb10a4da5b665
Jhoylan <jhoylangs@gmail.com>
2021-04-17 15:55:40 -0300

    Entries class

---
+++ entries.rb
@@ -0,0 +1,25 @@
+class Entries
+  def Entries.aks_for_a_row
+    puts 'Please, choose a row'
+    row = gets.chomp.to_i
+
+    while row < 1 || row > 3
+      puts 'please choose a valid row'
+      row = gets.chomp.to_i
+    end 
+
+    return row
+  end
+
+  def Entries.ask_for_a_column
+    puts 'Please, choose a column'
+    column = gets.chomp.to_i
+
+    while column < 1 || column > 3
+      puts 'please choose a valid column'
+      column = gets.chomp.to_i
+    end 
+
+    return column
+  end
+end
